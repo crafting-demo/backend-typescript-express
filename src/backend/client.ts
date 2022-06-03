430c58e70dfb1b384597b286ae89fa15e9116155
Alexander Maru <i@alexandermaru.com>
2022-06-03 00:24:04 +0000

    use default port names

--- src/backend/client.ts
+++ src/backend/client.ts
@@ -12,23 +12,23 @@ export class Client {
     switch (serviceType) {
       case ServiceType.Gin:
         host = process.env.GIN_SERVICE_HOST;
-        port = process.env.GIN_SERVICE_PORT_API;
+        port = process.env.GIN_SERVICE_PORT;
         break;
       case ServiceType.Express:
         host = process.env.EXPRESS_SERVICE_HOST;
-        port = process.env.EXPRESS_SERVICE_PORT_API;
+        port = process.env.EXPRESS_SERVICE_PORT;
         break;
       case ServiceType.Rails:
         host = process.env.RAILS_SERVICE_HOST;
-        port = process.env.RAILS_SERVICE_PORT_API;
+        port = process.env.RAILS_SERVICE_PORT;
         break;
       case ServiceType.Spring:
         host = process.env.SPRING_SERVICE_HOST;
-        port = process.env.SPRING_SERVICE_PORT_API;
+        port = process.env.SPRING_SERVICE_PORT;
         break;
       case ServiceType.Django:
         host = process.env.DJANGO_SERVICE_HOST;
-        port = process.env.DJANGO_SERVICE_PORT_API;
+        port = process.env.DJANGO_SERVICE_PORT;
         break;
       default:
         break;
